83dadc005f2b1cf070e2c81e70468907d82ab345
sergui20 <38196193+sergui20@users.noreply.github.com>
2019-03-11 14:40:55 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 const express = require('express'); // A web framework for Nodejs
 const app = express();
 
-const path = require('path'); // Its a node module included on its API, to make our paths relative. And supported in all OS
-const mongoose = require('mongoose'); // Elegant mongodb object modeling for nodejs
-const passport = require('passport'); // Passport is an authentication middleware for Node.js
-const flash = require('connect-flash'); // The flash is a special area of the session used for storing messages. Messages are written to the flash and cleared after being displayed to the user. The flash is typically used in combination with redirects, ensuring that the message is available to the next page that is to be rendered.
-const morgan = require('morgan'); // HTTP request logger middleware for node.js
-const cookieParser = require('cookie-parser'); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names. In a simple way, parse and store cokkies in req.cookies
-// const bodyParser = require('body-parser'); //Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
-const session = require('express-session'); // Store user data between http requests. It assigns the client an ID and it makes all further requests using that ID. Information associates with the client is stores on the server linked to that ID. It store the data in req.session
+const path = require('path');
+const mongoose = require('mongoose'); 
+const passport = require('passport'); 
+const flash = require('connect-flash'); 
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser'); 
+const session = require('express-session'); 
 
 const { url } = require('./config/database');
 
@@ -31,15 +30,15 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false})); // False because I dont want to process images and encode it
 app.use(session({
     secret: 'SNMC1999',
-    resave: false, // Forces the session to be saved back to the session store, even if the session was never modified during the request.
-    saveUninitialized: false // Forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified. 
+    resave: false,
+    saveUninitialized: false
 }));
-app.use(passport.initialize()); // Initialize passport
-app.use(passport.session()); // For persistent login sessions
-app.use(flash()); // To connect or communicate into different webpages and to send error or log messages between them
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 // Routes
-require('./app/routes')(app, passport); // We pass app and passport to use in that file. That file is a function with these two parameters
+require('./app/routes')(app, passport);
 
 // Static Files
 app.use(express.static(path.resolve(__dirname, 'public')));
